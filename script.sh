b847b57c95e9643607c13fbaccc75514eac36674
Haiping LE <haiping_le@bose.com>
2024-11-21 10:27:14 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
 FLATBUILD_NAME=""
 
 until [ ${#FLATBUILD_NAME} -gt 0 ]
 do
   echo "Script is checking for name"
-  FLATBUILD_NAME=$(buildkite-agent meta-data get flatbuild_name)
+  FLATBUILD_NAME=%buildkite-agent meta-data get flatbuild_name%
   echo "FLATBUILD_NAME is $NAME"
   sleep 5
 done
