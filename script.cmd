5a5643dbd58c63ecf39cea0147384064ca22f971
Haiping LE <haiping_le@bose.com>
2024-11-21 10:50:54 +0800

    Update script.cmd

--- script.cmd
+++ script.cmd
@@ -1,7 +1,7 @@
 @echo off
 :loop
 :: 使用 Buildkite Agent 获取 flatbuild_name
-for /f "delims=" %%i in ('buildkite-agent meta-data get flatbuild_name') do set FLATBUILD_NAME=%%i
+for /f "delims=" %%i in ('buildkite-agent meta-data get 'flatbuild_name' ') do set FLATBUILD_NAME=%%i
 
 :: 如果 FLATBUILD_NAME 为空，继续循环
 if not defined FLATBUILD_NAME (
