b1e790fc14657aed75e97e59f87679b40bebe47a
Haiping LE <haiping_le@bose.com>
2024-11-21 11:01:26 +0800

    Update script.cmd

--- script.cmd
+++ script.cmd
@@ -1,11 +1,11 @@
 @echo off
 :loop
 :: 使用 Buildkite Agent 获取 flatbuild_name
-for /f "delims=" %%i in ('buildkite-agent meta-data get 'flatbuild_name' ') do set FLATBUILD_NAME=%%i
+for /f "delims=" %%i in ('buildkite-agent meta-data get flatbuild_name') do set FLATBUILD_NAME=%%i
 
 :: 如果 FLATBUILD_NAME 为空，继续循环
 if not defined FLATBUILD_NAME (
-    echo Script is checking for name
+    echo 'Script is checking for name'
     timeout /t 5 >nul
     goto loop
 )
